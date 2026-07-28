35504caaa671652caf23ab32c4038d3de983954d
shruti0121 <shruti0121@server.ca>
2026-07-28 17:33:12 -0400

    Initial commit

---
+++ test/web_project_containerized.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WebProjectContainerized from '../lib/web_project_containerized-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/web_project_containerized-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WebProjectContainerized.WebProjectContainerizedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
